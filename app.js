30e1c4a76b87bc3550992c0b43f2d971d4c94704
yadavaryan17 <aryanyad174@gmail.com>
2025-01-29 00:46:29 +0530

    add star review functionality

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const MONGO_URL = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
 .then(() => {
